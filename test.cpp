063a0d24d9fb554d5ea3342626438d6c5e4b64b1
HanXiao <41134571+HanXiao68@users.noreply.github.com>
2021-04-21 17:45:15 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  C++_xcode
+//
+//  Created by H X on 2021/4/16.
+//
+
+#include <iostream>
+//#include <vector>
+#include<typeinfo>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    double a;
+    a = (double)25/2;
+    cout<<a<<endl;
+    printf("%f\n",a);
+    return 0;
+}
